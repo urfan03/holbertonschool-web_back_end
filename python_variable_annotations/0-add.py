4fba6bdf7338ddd3c4e60bd70cdef47ed71a96da
Urfan Musayev <83137111+urfan03@users.noreply.github.com>
2024-04-01 14:48:58 +0400

    holberton

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python3
-
-
 '''
     Write a type-annotated function add
     that takes a float a and a float b
@@ -9,5 +7,5 @@
 
 
 def add(a:float, b:float) -> float:
-
+    """Return sum of a and b"""
     return a + b
