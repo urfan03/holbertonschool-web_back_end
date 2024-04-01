420f472bf96b36825510932ec963a50c7dd88909
Urfan Musayev <83137111+urfan03@users.noreply.github.com>
2024-04-01 16:27:51 +0400

    holberton

--- python_variable_annotations/100-safe_first_element.py
+++ python_variable_annotations/100-safe_first_element.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
-'''
-Augment the following code with
-the correct duck-typed annotations:
-'''
+""" 10. Duck typing - first element of a sequence
+"""
 
 from typing import Any, Sequence, Union
 
 
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
-    """duck-typed annotations"""
+    """ Gets first element safely.
+    """
     if lst:
         return lst[0]
     else:
