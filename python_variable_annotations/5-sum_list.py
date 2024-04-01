8e68febf6c08b71a40406584931530281b0faa7a
Urfan Musayev <83137111+urfan03@users.noreply.github.com>
2024-04-01 15:19:27 +0400

    holberton

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -8,4 +8,4 @@ argument and returns their sum as a float.
 
 def sum_list(input_list: list[float]) -> float:
     """returns their sum as a float"""
-    return sum(sum_list[float])
+    return sum(input_list)
