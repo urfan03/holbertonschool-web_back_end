5a48ad5c1f64847458df6c9d711328bc953f8896
Urfan Musayev <83137111+urfan03@users.noreply.github.com>
2024-04-01 14:57:58 +0400

    holberton

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -5,6 +5,7 @@ that takes a string str1 and a string str2
 as arguments and returns a concatenated string
 '''
 
+
 def concat(str1: str, str2: str) -> str:
     """sum of a string str1 and a string str2"""
     return str1 + str2
