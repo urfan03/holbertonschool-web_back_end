6bb24d02ccc80310e34d78f5f6c7b310b1d0fee6
Urfan Musayev <83137111+urfan03@users.noreply.github.com>
2024-04-01 15:13:05 +0400

    done

--- python_variable_annotations/4-define_variables.py
+++ python_variable_annotations/4-define_variables.py
@@ -7,4 +7,4 @@ Define and annotate the following variables with the specified values
 a: int = 1
 pi: float = 3.14
 i_understand_annotations: bool = True
-school: str="Holberton"
+school: str = "Holberton"
