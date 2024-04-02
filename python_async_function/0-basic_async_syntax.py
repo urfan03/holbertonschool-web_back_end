fb0e3a48479d15f0e358357c4da4c2e9b55a92de
Urfan Musayev <83137111+urfan03@users.noreply.github.com>
2024-04-02 09:33:25 +0400

    holberton

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Write an asynchronous coroutine that takes in an integer argument
+"""
+
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int=10) -> float:
+    """Use the random module"""
+    delay=max_delay*random.random()
+    await asyncio.sleep(delay)
+    return (delay)
