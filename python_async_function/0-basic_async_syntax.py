2e3723e5817605e26adf58c6ec00a49281147bd3
Urfan Musayev <83137111+urfan03@users.noreply.github.com>
2024-04-02 09:45:17 +0400

    holberton

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -7,8 +7,8 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay: int=10) -> float:
+async def wait_random(max_delay: int = 10) -> float:
     """Use the random module"""
-    delay=max_delay*random.random()
+    delay=max_delay * random.random()
     await asyncio.sleep(delay)
     return (delay)
