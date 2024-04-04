5880b6630a96c9938ab9cc4de3b85c04fd1fdc39
urfan03 <urfanmusayev2003@gmail.com>
2024-04-04 17:10:27 +0400

    holberton

---
+++ python_async_function/2-measure_runtime.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+'''
+Create a measure_time function with integers n and max_delay as arguments
+'''
+
+from asyncio import run
+from time import time
+wait_n = __import__('1-cocurrent_coroutines').wait_n
+
+
+async def measure_time(n: int, max_delay: int = 10) -> float:
+    """Use the time module to measure an approximate elapsed time."""
+    time_1 = time()
+    run(wait_n(n, max_delay))
+    time_2 = time()
+    final_time = time_2 - time_1
+    return final_time/n
