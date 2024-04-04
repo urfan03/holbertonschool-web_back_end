7480b4b27d4fc4ae866e15c4339f3ff42a2bc37e
urfan03 <urfanmusayev2003@gmail.com>
2024-04-04 17:40:03 +0400

    holberton

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-''' Description: Write a function (do not create an async function, use
-                 the regular function syntax to do this) task_wait_random
-                 that takes an integer max_delay and returns a asyncio.Task.
-    Arguments: max_delay: int
-'''
+""" From the previous file, import wait_n into 2-measure_runtime.py.
+    Create a measure_time function with integers n and max_delay as arguments
+    that measures the total execution time for wait_n(n, max_delay), and
+    returns total_time / n. Your function should return a float.
+    Use the time module to measure an approximate elapsed time."""
 
 import asyncio
 
---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+""" Take the code from wait_n and alter
+it into a new function task_wait_n.
+The code is nearly identical to wait_n except
+task_wait_random is being called."""
+
+from typing import List
+import asyncio
+import random
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int = 10) -> List[float]:
+    '''Execute task_wait_random and returns sorted list of delay'''
+    spawn_ls = []
+    delay_ls = []
+    for i in range(n):
+        delayed_task = task_wait_random(max_delay)
+        delayed_task.add_done_callback(lambda x: delay_ls.append(x.result()))
+        spawn_ls.append(delayed_task)
+
+    for spawn in spawn_ls:
+        await spawn
+
+    return delay_ls
