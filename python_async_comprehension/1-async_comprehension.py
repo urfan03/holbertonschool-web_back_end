433a284180c2a7601ff87d2d5834fa971c17f6aa
urfan03 <urfanmusayev2003@gmail.com>
2024-04-06 00:22:52 +0400

    holberton

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -3,7 +3,6 @@
 using an async comprehensing over async_generator,
 then return the 10 random numbers."""
 
-import asyncio
 from typing import List
 
 async_generator=__import__('0-async_generator').async_generator
