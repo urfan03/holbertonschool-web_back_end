d95850434fe22295a10e38f6f5a0d71d428c500a
urfan03 <urfanmusayev2003@gmail.com>
2024-04-08 17:40:53 +0400

    holberton

--- caching/1-fifo_cache.py
+++ caching/1-fifo_cache.py
@@ -1,20 +1,23 @@
 #!/usr/bin/python3
-"""  Basic dictionary """
+""" FIFO caching """
 from base_caching import BaseCaching
 
+
 class FIFOCache(BaseCaching):
+    """ Class that inherits from BaseCaching and is a caching system """
     def __init__(self):
         super().__init__()
         self.data = {}
         self.next_in, self.next_out = 0, 0
 
-
     def _pop(self):
+        """ FIFO algorithm, remove element """
         self.next_out += 1
-        key = self.data(self.next_out)
+        key = self.data[self.next_out]
         del self.data[self.next_out], self.cache_data[key]
 
     def _push(self, key, item):
+        """ FIFO algorithm, add element """
         if len(self.cache_data) > BaseCaching.MAX_ITEMS - 1:
             print("DISCARD: {}".format(self.data[self.next_out + 1]))
             self._pop()
@@ -23,6 +26,7 @@ class FIFOCache(BaseCaching):
         self.data[self.next_in] = key
 
     def put(self, key, item):
+        """ Assign to the dictionary """
         if key and item:
             if key in self.cache_data:
                 self.cache_data[key] = item
@@ -30,8 +34,9 @@ class FIFOCache(BaseCaching):
                 self._push(key, item)
 
     def get(self, key):
-        if key is None or self.cache_data(key) is None:
+        """ Return the value linked """
+        if key is None or self.cache_data.get(key) is None:
             return None
         if key in self.cache_data:
-            value  = self.cache_data[key]
+            value = self.cache_data[key]
             return value
