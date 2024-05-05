2d23a5f5d169e03c201f8c07efe97af99d869666
urfan03 <urfanmusayev2003@gmail.com>
2024-05-05 18:34:13 +0400

    holberton

--- i18n/4-app.py
+++ i18n/4-app.py
@@ -2,7 +2,7 @@
 """ Basic Flask app, Basic Babel setup, Get locale from request,
     Parametrize templates, Force locale with URL parameter """
 from flask import Flask, render_template, request
-from flask_babel import Babel, gettext
+from flask_babel import Babel
 
 app = Flask(__name__)
 babel = Babel(app)
