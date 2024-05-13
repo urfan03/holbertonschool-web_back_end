7a5a0586f27587d2fee7c0b2513cbe8b36b66b0d
urfan03 <urfanmusayev2003@gmail.com>
2024-05-13 17:03:53 +0400

    holberton

--- 0x0B_redis_basic/main.py
+++ 0x0B_redis_basic/main.py
@@ -8,10 +8,9 @@ Cache = __import__('exercise').Cache
 
 cache = Cache()
 
-data = b"hello"
-key = cache.store(data)
-print(key)
+cache.store(b"first")
+print(cache.get(cache.store.__qualname__))
 
-local_redis = redis.Redis()
-print(local_redis.get(key))
-
+cache.store(b"second")
+cache.store(b"third")
+print(cache.get(cache.store.__qualname__))
