19c24a1aadcdcef9cd385fbe47eaac92bd551fe9
urfan03 <urfanmusayev2003@gmail.com>
2024-05-13 17:10:11 +0400

    holberton

--- 0x0B_redis_basic/web.py
+++ 0x0B_redis_basic/web.py
@@ -1,31 +1,28 @@
-#!/usr/bin/env python3
-""" Advanced - Module for Implementing an expiring
-    web cache and tracker
-"""
 
+#!/usr/bin/env python3
+""" Module for Implementing an expiring web cache and tracker """
+
+from functools import wraps
 import redis
 import requests
 from typing import Callable
-from functools import wraps
 
-rd = redis.Redis()
+client = redis.Redis()
 
 
 def count_requests(method: Callable) -> Callable:
-    """ Counting with decorators how many times a request
-        has been made
-    """
+    """ Decortator to count how many request has been made"""
 
     @wraps(method)
     def wrapper(url):
-        """ Wrapper for decorator functionality """
-        rd.incr(f"count:{url}")
-        cached_html = rd.get(f"cached:{url}")
+        """ Function wrapper """
+        client.incr(f"count:{url}")
+        cached_html = client.get(f"cached:{url}")
         if cached_html:
             return cached_html.decode('utf-8')
 
         html = method(url)
-        rd.setex(f"cached:{url}", 10, html)
+        client.setex(f"cached:{url}", 10, html)
         return html
 
     return wrapper
@@ -33,8 +30,7 @@ def count_requests(method: Callable) -> Callable:
 
 @count_requests
 def get_page(url: str) -> str:
-    """ requests module to obtain the HTML
-        content of a particular URL and returns it.
+    """Gets the html content of a web page
     """
     req = requests.get(url)
     return req.text
