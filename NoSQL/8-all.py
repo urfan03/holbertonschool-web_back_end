8d07ea0b937f8147bedd30c234ce95f56bc1c370
urfan03 <urfanmusayev2003@gmail.com>
2024-05-26 03:26:46 +0400

    holberton

---
+++ NoSQL/8-all.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" list all pymongo"""
+
+
+def list_all(mongo_collection):
+    """list all documents in a collection"""
+    documents = mongo_collection.find()
+    return documents
---
+++ NoSQL/9-insert_school.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""pymongo"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """ inserts a new document in a mongo collection"""
+    new_documents = mongo_collection.find(kwargs)
+    return new_documents.inserted_id
