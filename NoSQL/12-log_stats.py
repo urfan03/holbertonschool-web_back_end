7b8ae833df23b2734bed5047b32fdd1a0ff19e7d
urfan03 <urfanmmusayev2003@gmail.com>
2024-05-26 21:32:21 +0400

    done

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,20 +1,31 @@
 #!/usr/bin/env python3
-"""Print info in a collection"""
+"""log stats from collection
+"""
 from pymongo import MongoClient
 
-if __name__ == "__main__":
-    """ Make a checkk for all elements in a collention """
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    collection = client.logs.nginx
 
-    print(f"{collection.estimated_document_count()} logs")
+METHODS = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
+
+def log_stats(mongo_collection, option=None):
+    """ script that provides some stats about Nginx logs stored in MongoDB
+    """
+    items = {}
+    if option:
+        value = mongo_collection.count_documents(
+            {"method": {"$regex": option}})
+        print(f"\tmethod {option}: {value}")
+        return
+
+    result = mongo_collection.count_documents(items)
+    print(f"{result} logs")
     print("Methods:")
-    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
-        method_count = collection.count_documents({'method': method})
-        print(f"\tmethod {method}: {method_count}")
+    for method in METHODS:
+        log_stats(nginx_collection, method)
+    status_check = mongo_collection.count_documents({"path": "/status"})
+    print(f"{status_check} status check")
 
-    check_get = collection.count_documents({
-        'method': 'GET', 'path': "/status"
-    })
-    print(f"{check_get} status check")
+
+if __name__ == "__main__":
+    nginx_collection = MongoClient('mongodb://127.0.0.1:27017').logs.nginx
+    log_stats(nginx_collection)
