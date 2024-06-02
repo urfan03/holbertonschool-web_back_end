a2a85e94fc7d6bfcf74f8f977f2d6e6c211113a9
urfan03 <urfanmmusayev2003@gmail.com>
2024-06-02 15:43:19 +0400

    done

---
+++ ES6_classes/1-make_classrooms.js
@@ -0,0 +1,8 @@
+import ClassRoom from "./0-classroom";
+const initializeRooms = () => {
+    const array = [19, 20, 34];
+
+    return array.map((n) => new ClassRoom(n));
+};
+
+export default initializeRooms;
