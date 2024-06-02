f046def5a70954ec405ed90510131deb1ef680cc
urfan03 <urfanmmusayev2003@gmail.com>
2024-06-02 15:57:59 +0400

    done

--- ES6_classes/0-classroom.js
+++ ES6_classes/0-classroom.js
@@ -1,7 +1,9 @@
-/**You used to attend a place like this at some point */
-
+/* Classroom
+    https://stackoverflow.com/questions/4484424/underscore-prefix-for-property-and-method-names-in-javascript
+    prefix
+*/
 export default class ClassRoom {
-    constructor(maxStudentsSize) {
-        this._maxStudentsSize = maxStudentsSize;
+    constructor(maxStudentsSize = 0) {
+      this._maxStudentsSize = maxStudentsSize;
     }
 }
