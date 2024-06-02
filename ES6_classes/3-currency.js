12956faabc1b7842efd6d40c169526e7ccbfc4ae
urfan03 <urfanmmusayev2003@gmail.com>
2024-06-02 16:03:20 +0400

    done

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,26 +1,37 @@
+/*
+    Make method
+    This can't with arrow,
+    because arrow function doesn't work with this
+*/
 export default class Currency {
-    constructor(code, name) {
-      this._code = code;
-      this._name = name;
+    constructor(code = '', name = '') {
+      this.code = code;
+      this.name = name;
     }
 
-    set code(code) {
-      this._code = code;
+    displayFullCurrency() {
+      return `${this.name} (${this.code})`;
     }
 
     get code() {
       return this._code;
     }
 
-    set name(name) {
-      this._name = name;
+    set code(value) {
+      if (typeof value !== 'string') {
+        throw new TypeError('Code must be a string');
+      }
+      this._code = value;
     }
 
     get name() {
       return this._name;
     }
 
-    displayFullCurrency() {
-      return `${this._name} (${this._code})`;
+    set name(value) {
+      if (typeof value !== 'string') {
+        throw new TypeError('Name must be a string');
+      }
+      this._name = value;
     }
 }
--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -1,30 +1,51 @@
+// Pricing
+import Currency from './3-currency';
+
 export default class Pricing {
-    constructor(amount, currency) {
-      this._amount = amount;
-      this._currency = currency;
+  constructor(amount = '', currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  displayFullPrice() {
+    const code = (this.currency._code);
+    const name = (this.currency._name);
+    const money = `${this.amount} ${name} (${code})`;
+
+    return money;
+  }
+
+  static convertPrice(amount = 0, conversionRate = 0) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('amount must be a string');
     }
 
-    set amount(amount) {
-      this._amount = amount;
+    if (typeof conversionRate !== 'number') {
+      throw new TypeError('conversionRate must be a string');
     }
 
-    get amount() {
-      return this._amount;
-    }
+    return (amount * conversionRate);
+  }
 
-    set currency(currency) {
-      this._currency = currency;
-    }
+  get amount() {
+    return this._amount;
+  }
 
-    get currency() {
-      return this._currency;
+  set amount(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('amount must be a number');
     }
+    this._amount = value;
+  }
 
-    displayFullPrice() {
-      return `${this.amount} ${this.currency.displayFullCurrency()}`;
-    }
+  get currency() {
+    return this._currency;
+  }
 
-    static convertPrice(amount, conversionRate) {
-      return amount * conversionRate;
+  set currency(value) {
+    if (!(value instanceof Currency)) {
+      throw new TypeError('currency must be a Currency');
     }
+    this._currency = value;
+  }
 }
