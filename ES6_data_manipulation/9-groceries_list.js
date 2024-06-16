440a00f2159cd25ce2d6bfadcfdc1bc883395adc
urfan03 <urfanmusayev2003@gmail.com>
2024-06-16 22:08:40 +0400

    done

--- ES6_data_manipulation/9-groceries_list.js
+++ ES6_data_manipulation/9-groceries_list.js
@@ -1,11 +1,12 @@
-const groceriesList = () => {
-    const groceries = new Map();
-    groceries.set('Apples', 10);
-    groceries.set('Tomatoes', 10);
-    groceries.set('Pasta', 1);
-    groceries.set('Rice', 1);
-    groceries.set('Banana', 5);
-    return groceries;
-};
+function groceriesList() {
+    return new Map([
+      ['Apples', 10],
+      ['Tomatoes', 10],
+      ['Pasta', 1],
+      ['Rice', 1],
+      ['Banana', 5],
+    ]);
+  }
 
-export default groceriesList;
+  export default groceriesList;
+  
